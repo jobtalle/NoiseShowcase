d1408f65325357d64c0bafc608442491f230766f
Job <jobtalle@hotmail.com>
2017-10-26 15:50:51 +0200

    Implemented falloff & octaves

--- js/renderCubicNoise.js
+++ js/renderCubicNoise.js
@@ -1,6 +1,5 @@
 function cubicNoiseSetup() {
 	document.getElementById("cubic-noise-randomize-seed").click();
-	document.getElementById("cubic-noise-generate").click();
 }
 
 function cubicNoiseRender() {
@@ -12,21 +11,48 @@ function cubicNoiseRender() {
 		
 		context.clearRect(0, 0, canvas.width, canvas.height);
 		
+		var result = new Image();
 		var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-		var seed = document.getElementById("cubic-noise-seed").value;
-		var period = document.getElementById("cubic-noise-period").value;
-		var config = cubicNoiseConfig(seed, period);
+		var seed = parseFloat(document.getElementById("cubic-noise-seed").value);
+		var quality = parseFloat(1 << (5 - document.getElementById("cubic-noise-quality").value));
+		var period = parseFloat(document.getElementById("cubic-noise-period").value / quality);
+		var octaves = parseFloat(document.getElementById("cubic-noise-octaves").value);
+		var falloff = parseFloat(document.getElementById("cubic-noise-falloff").value);
 		
-		for(var y = 0; y < canvas.height; ++y) {
-			for(var x = 0; x < canvas.width; ++x) {
-				var index = (x + y * canvas.width) * 4;
-				var value = cubicNoiseSample(config, x, y) * 255;
-				
-				imageData.data[index] = value;
-				imageData.data[index + 1] = value;
-				imageData.data[index + 2] = value;
-				imageData.data[index + 3] = 255;
+		var divisor = 0;
+		var m = 1;
+		for(var octave = 0; octave < octaves; ++octave) {
+			divisor += m;
+			m *= falloff;
+		}
+		
+		for(var octave = 0; octave < octaves; ++octave) {
+			var amplitude = (1 / Math.pow(falloff, octave + 1)) * (Math.pow(falloff, octaves) / divisor);
+			var config = cubicNoiseConfig(seed, period / (octave + 1));
+			
+			for(var y = 0; y < Math.floor(canvas.height / quality); ++y) {
+				for(var x = 0; x < Math.floor(canvas.width / quality); ++x) {
+					var index = (x + y * canvas.width) * quality;
+					var value = cubicNoiseSample(config, x, y) * 255 * amplitude;
+					
+					for(var yrep = 0; yrep < quality; ++yrep) {
+						for(var xrep = 0; xrep < quality; ++xrep) {
+							
+							var repIndex = (index + xrep + yrep * canvas.width) * 4;
+							
+							imageData.data[repIndex] += value;
+							imageData.data[repIndex + 1] += value;
+							imageData.data[repIndex + 2] += value;
+							imageData.data[repIndex + 3] = 255;
+						}
+					}
+				}
 			}
+			
+			period /= 2;
+			
+			if(period < 1)
+				break;
 		}
 		
 		context.putImageData(imageData, 0, 0);
@@ -35,4 +61,6 @@ function cubicNoiseRender() {
 
 function cubicNoiseRandomizeSeed() {
 	document.getElementById("cubic-noise-seed").value = Math.round(4294967295 * Math.random());
+	
+	cubicNoiseRender();
 }
